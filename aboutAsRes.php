c537225daf9ad9271c2807119a66be3b2558be8d
Sunandan9 <sunandan0709.be20@chitkara.edu.in>
2022-09-04 16:26:24 +0530

    commit

---
+++ aboutAsRes.php
@@ -0,0 +1,117 @@
+<?php
+/*
+if(isset($_POST['username'])){ */
+    $server = "localhost";
+   $username = "root";
+   $password = "";
+   $database = "registerc";
+   
+   $con = mysqli_connect($server,$username,$password,$database);
+   
+   if(!$con){
+       die("connection to this database is failed due to" . mysqli_connect_error());
+   }
+   
+
+   
+
+   
+
+   $res = 2;
+   $username=$_POST['username'];
+   //echo $username;
+   $password=$_POST['password']; 
+  // echo $password; 
+   $sql = "SELECT * FROM `registerr`";
+ //  mysql_select_db('registerc');
+   $retval = mysqli_query($con, $sql );
+   $num = mysqli_num_rows($retval);
+   if($num>0)
+   {
+      while($row = mysqli_fetch_assoc($retval)){
+      if($username == $row['username'])
+      {
+         if($password == $row['password'])
+         {
+            $res = 1;
+            header("Location:http://localhost/foodshala/loginAsResto.html");
+         }
+         else{
+            $res = 0;
+         }
+      }
+      echo "<br>";
+      echo "<br>";
+      }
+   }
+   if($res == 0){
+      echo '<script>alert("Incorrect Password or username")</script>';
+   }
+   
+ /*  if(! $retval ) {
+       echo "success";
+  // die('Could not get data: ' . mysqli_error());
+   }
+  else{
+  echo "not success";
+  }
+
+while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
+   echo "success dhairya";
+}
+
+
+   if($con->query($sql)==true){
+       echo"sucessfully inserted";
+   }
+   else{
+       echo "ERROR: $sql <br> $con->error";
+   }
+   
+   $con->close();*/
+  // }
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> Login or Register </title>
+<link rel="stylesheet" href="style.css">
+<style>
+body{
+   background-image:url('login.png');
+}
+</style>
+</head>
+<body>
+  
+ <div class="login-page"> 
+ <div class="form">
+ <form class="login-form" action="aboutAsRes.php" method="post">
+ <input type="text" name="username" placeholder="Username" required>
+ <input type="password" name="password" placeholder="Password" required>
+ <!--<form action="index.html"> -->
+ <button>login
+ </button>
+ <!-- </form>-->
+ </div>
+ </div>
+
+
+    <script src='https://code.jquery.com/jquery-3.2.1.min.js'> </script>
+ 
+ <script>
+ $('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    });
+ 
+ </script>
+</body>
+</html>
+ 
---
+++ blogAsRes.php
@@ -0,0 +1,89 @@
+<?php
+
+
+if(isset($_POST['username'])){ 
+ $server = "localhost";
+$username = "root";
+$password = "";
+
+$con = mysqli_connect($server,$username,$password);
+
+if(!$con){
+    die("connection to this database is failed due to" . mysqli_connect_error());
+}
+
+//echo "success connecting to the db";
+
+$username=$_POST['username'];
+$password=$_POST['password'];
+$confirmpass=$_POST['confirmpass'];
+$emailid=$_POST['emailid'];
+$number=$_POST['number'];
+$pref=$_POST['pref'];
+
+$sql= "INSERT INTO `registerc`.`registerr` (`username`, `password`, `confirmpassword`, `emailid`, `mobileno`, `veg/nonveg`, `dt`) 
+VALUES ('$username', '$password', '$confirmpass', '$emailid', '$number', '$pref', current_timestamp());";
+//alert("username");
+//echo 'Your name is ' . $username;
+if($con->query($sql)==true){
+   // echo"sucessfully inserted";
+}
+else{
+    echo "ERROR: $sql <br> $con->error";
+}
+
+$con->close();
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> Login or Register </title>
+<link rel="stylesheet" href="style.css">
+<style>
+body{
+   background-image:url('login.png');
+}
+</style>
+</head>
+<body>
+  
+ <div class="login-page"> 
+ <div class="form">
+      <form action="blogAsRes.php" method="post"> 
+   <input type="text" name="username" placeholder="Username" required>
+   <input type="password" name="password" id="p" placeholder="Password" required>
+   <input type="password" name="confirmpass" id="confirm_p"placeholder="Confirm password" onblur="confirmPass" required>
+   <script type="text/javascript">
+        function confirmPass(){
+		var pass=document.getElementById("p").value;
+		var Cpass=document.getElementById("confirm_p").value;
+		if(pass!=Cpass)
+		    alert('Wrong confirm password');
+		}
+   </script>
+   <input type="Email" name="emailid" placeholder="Email ID" required>
+   <input type="number" name="number" placeholder="Mobile Number" maxlength="10">
+   <input type="text"  name="pref" placeholder="Veg/Non-Veg" required>
+   <button>Create</button><br>
+   </form>
+    
+
+ </div>
+ </div>
+
+
+    <script src='https://code.jquery.com/jquery-3.2.1.min.js'> </script>
+ 
+ <script>
+ $('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    });
+ 
+ </script>
+</body>
+</html>
+ 
